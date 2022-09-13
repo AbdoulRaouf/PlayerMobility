51f2332567ccc1a7ace068bf1c78becfd4447353
Appi72020 <abdoulraouf72020@gmail.com>
2022-09-13 12:42:18 +0100

    Movement optimisation
    Star coding Jump

--- PlayerMobility/Assets/_Scripts/PlayerController.cs
+++ PlayerMobility/Assets/_Scripts/PlayerController.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _jumpforce;
     [SerializeField] private Rigidbody _rb;
+    [SerializeField] private bool _canjump;
+    [SerializeField] private bool _isjumping;
+    [SerializeField] private float _downforce;
     void Start()
     {
         
@@ -13,8 +18,24 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        //if (Input.GetKeyUp(KeyCode.Joystick1Button0)) transform.position+=Vector3.forward;
-        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        _rb.velocity = direction * _speed;
+
+        Vector3 VelocityDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * _speed;
+
+        _rb.velocity = VelocityDirection;
+
+        VelocityDirection.y=_rb.velocity.y;
+
+        if (Input.GetKeyUp(KeyCode.Joystick1Button0) && _canjump)
+        {
+            _isjumping = true;
+            _canjump = false;
+            
+            _rb.AddForce(Vector3.up * _jumpforce);
+
+
+            if(transform.position.y<1) _canjump = true;
+        
+        
+        }
     }
 }
