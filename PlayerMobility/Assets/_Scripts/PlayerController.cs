55838f3d84f60d6c10469fbec1a911f27994a531
Appi72020 <abdoulraouf72020@gmail.com>
2022-09-13 16:48:32 +0100

    Update PlayerController.cs

--- PlayerMobility/Assets/_Scripts/PlayerController.cs
+++ PlayerMobility/Assets/_Scripts/PlayerController.cs
@@ -8,9 +8,6 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpforce;
     [SerializeField] private Rigidbody _rb;
-    [SerializeField] private bool _canjump;
-    [SerializeField] private bool _isjumping;
-    [SerializeField] private float _downforce;
     void Start()
     {
         
@@ -25,17 +22,6 @@ public class PlayerController : MonoBehaviour
 
         VelocityDirection.y=_rb.velocity.y;
 
-        if (Input.GetKeyUp(KeyCode.Joystick1Button0) && _canjump)
-        {
-            _isjumping = true;
-            _canjump = false;
-            
-            _rb.AddForce(Vector3.up * _jumpforce);
-
-
-            if(transform.position.y<1) _canjump = true;
-        
-        
-        }
+        if (Input.GetKeyDown(KeyCode.Joystick1Button0))_rb.AddForce(Vector3.up * _jumpforce);
     }
 }
